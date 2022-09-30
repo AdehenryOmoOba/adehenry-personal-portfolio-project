b8136dcc5ca9c2a781d84d1dffee2f7f966488d1
adehenry <adefemiadelaja@gmail.com>
2022-09-30 07:04:52 -0700

    contact section

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Toggle menu 
-let toggleMenu = document.querySelector(".toggle-menu");
+const toggleMenu = document.querySelector(".toggle-menu");
+const navbar = document.querySelector(".navbar");
+
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("active");
+  navbar.classList.toggle('active')
+});
+
+navbar.addEventListener("click", (e) => {
+  if (e.target.localName === 'a') {
+  navbar.classList.remove("active")
+  toggleMenu.classList.remove("active")}
 });
